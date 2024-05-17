1e1bd431e41c69298599b33732d575225c14d421
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2024-05-17 15:00:23 +0400

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,13 +1,9 @@
-start, end = 100, 200
+def pascal_triangle(n):
+    row = [1]
+    y = [0]
+    for _ in range(max(n, 0)):
+        print(row)
+        row = list(left + right for left, right in zip(row + y, y + row))
 
 
-def get_largest_palindromic(st, en):
-    if st < en:
-        st, en = en, st
-    for current in range(st, en, -1):
-        if str(current) == str(current)[::-1]:
-            return current
-    return None
-
-
-print(get_largest_palindromic(start, end))
+pascal_triangle(8)
