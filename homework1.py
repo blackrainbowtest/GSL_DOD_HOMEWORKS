2a266d30d8b51b62697a4e9220b786cc541ee3e7
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2024-05-17 15:00:38 +0400

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,3 +1,2 @@
-tuples_list = [(1, 2, 3), (4, 1, 2), (2, 2, 2)]
-# get max using key=sum
-print(max(tuples_list, key=sum))
+a = [89, 1, 60, 3, 89, 8, 13, 75, 21, 34, 1, 45, 55, 2, 15, 5]
+print(list(filter(lambda x: not x % 15, a)))
