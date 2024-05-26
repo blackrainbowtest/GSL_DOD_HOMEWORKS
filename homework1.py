eaaad9453bacdbdc70e65afb6449ca88f87ed468
Aram <76160385+blackrainbowtest@users.noreply.github.com>
2024-05-26 20:23:13 +0400

    Update homework1.py

--- homework1.py
+++ homework1.py
@@ -1,12 +1,11 @@
-import math
-
 while True:
-    u_input = input('Enter your word (or "q" to quit): ')
+    u_input = input('Enter nums with spaces (or "q" to quit): ')
     if u_input.lower() == 'q':
         break
     try:
-        # On input Euphoria we get EHA (not EOA) bc len() - 1 == [-1]
-        new_str = u_input[0] + u_input[math.ceil(len(u_input) / 2) - 1] + u_input[-1]
-        print(new_str.upper())
+        u_array = list(map(int, u_input.split(' ')))
+        u_array.insert(0, u_array[-1])
+        u_array.pop()
+        print(u_array)
     except ValueError:
         print('Uncorrected input, try again.')
